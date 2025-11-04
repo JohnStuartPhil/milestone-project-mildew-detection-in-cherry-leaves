3aef1754ab2e5e0aa59f9b0849c94cf709e7f208
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-11-04 21:45:24 +0000

    Add initial code to page_project_hyothesis.py

---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"* Sentence 1, "
+        f"continued. \n\n"
+        f"* Sentence 2. "
+        f"continued "
+        f"continued."
+
+    )
